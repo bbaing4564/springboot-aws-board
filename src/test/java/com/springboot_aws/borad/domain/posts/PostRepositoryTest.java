58867ea9d4ad8c58f5b967ac9ad44beeaf54b6e3
bbaing4564 <bbaing4564@gmail.com>
2020-04-06 00:09:06 +0900

    apply JPA to project

---
+++ src/test/java/com/springboot_aws/borad/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.springboot_aws.borad.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @After
+    public void cleanup() {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글_저장_불러오기() {
+        //given
+        String title = "test_title";
+        String content = "test_content";
+
+        postRepository.save(Posts.builder()
+            .title(title)
+            .content(content)
+            .author("bbaing4564@gmail.com")
+            .build());
+
+        //when
+        List<Posts> postsList = postRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
