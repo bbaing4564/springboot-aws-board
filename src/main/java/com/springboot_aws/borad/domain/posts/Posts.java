f4011cfd814b8fa99c67b2b35de636f19f3099e3
bbaing4564 <bbaing4564@gmail.com>
2020-04-07 00:36:28 +0900

    add BaseTimeEntity to using JPA Auditing

--- src/main/java/com/springboot_aws/borad/domain/posts/Posts.java
+++ src/main/java/com/springboot_aws/borad/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.springboot_aws.borad.domain.posts;
 
+import com.springboot_aws.borad.web.dto.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
