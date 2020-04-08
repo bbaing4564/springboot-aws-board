9571327d2cb1ac7a8cb58ef5c0065a04ee897994
bbaing4564 <bbaing4564@gmail.com>
2020-04-08 23:29:16 +0900

    test mustache templates

---
+++ src/main/java/com/springboot_aws/borad/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.springboot_aws.borad.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String idnex() {
+        return "index";
+    }
+}
