2b27d040b658cf462a59a8d3d4c7b117da6364b8
bbaing4564 <bbaing4564@gmail.com>
2020-04-15 08:21:26 +0900

    apply spring-security on tests

--- src/test/java/com/springboot_aws/borad/web/HelloControllerTest.java
+++ src/test/java/com/springboot_aws/borad/web/HelloControllerTest.java
@@ -1,11 +1,13 @@
-package com.springboot_aws.borad;
+package com.springboot_aws.borad.web;
 
-import com.jayway.jsonpath.JsonPath;
-import com.springboot_aws.borad.web.HelloController;
+import com.springboot_aws.borad.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,13 +16,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles="USER")
     public void return_hello() throws Exception {
         String hello = "hello";
 
@@ -30,6 +36,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void return_helloDto() throws Exception {
         String name = "hello";
         int amount = 1000;
